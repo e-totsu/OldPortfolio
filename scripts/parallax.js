0ab7a949d8949650a2fc8a13a6b601856024bd2d
Kamil Sohlich <kamilsohlich@gmail.com>
2022-03-24 21:09:33 +0100

    Fixes, optimizations and new elements in the Contact section

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,9 +1,21 @@
+const root      = document.querySelector(':root')
+const container = document.querySelector('.con-me')
+
 window.addEventListener('scroll', () => {
-  const distance = (window.scrollY / 2) * .01;
+  if (window.innerWidth > 768) {
+    const distance = window.scrollY * .005
 
-  const root = document.querySelector(':root')
-  const container = document.querySelector('.con-me')
+    root.style.setProperty     ("--con-me-size", `${ 1 + distance }`)
+    container.style.setProperty('top'          , `${ distance * 4  }rem`)
+  } else {
+    root.style.setProperty     ("--con-me-size", `1`)
+    container.style.setProperty('top'          , `0`)
+  }
+})
 
-  root.style.setProperty     ("--con-me-size", `${ 1 + distance }`);
-  container.style.setProperty('top'          , `${ distance * 4  }rem`)
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    root.style.setProperty     ("--con-me-size", `1`)
+    container.style.setProperty('top'          , `0`)
+  }
 })
